03776e9b3786de062fa2a2df6158e5c8047d47f8
Flavio Corpa <flaviocorpa@gmail.com>
2025-10-27 13:00:00 +0100

    almost done!!

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,16 +1,59 @@
 module Tests exposing (all)
 
 import Expect
+import List.Zipper as Zipper exposing (Zipper)
+import Main exposing (Province, Status(..), getProvinceStatus)
 import Test exposing (Test, describe, test)
+
+
+alicante : Province
+alicante =
+    { name = "Alicante", id = 2, status = NotAsked }
+
+
+madrid : Province
+madrid =
+    { name = "Madrid", id = 1, status = Correct }
+
+
+valencia : Province
+valencia =
+    { name = "Valencia", id = 3, status = Failed }
 
 
 all : Test
 all =
-    describe "A Test Suite"
-        [ test "Addition" <|
+    describe "getProvinceStatus tests"
+        [ test "current province is focused -> Focused" <|
             \_ ->
-                Expect.equal 10 (3 + 7)
-        , test "String.left" <|
+                let
+                    zipper : Zipper Province
+                    zipper =
+                        Zipper.singleton alicante
+                in
+                Expect.equal Focused (getProvinceStatus 2 zipper)
+        , test "province not yet asked -> NotAsked" <|
             \_ ->
-                Expect.equal "a" (String.left 1 "abcdefg")
+                let
+                    zipper : Zipper Province
+                    zipper =
+                        Zipper.singleton alicante
+                in
+                Expect.equal NotAsked (getProvinceStatus 1 zipper)
+        , test "province asked and answered correctly -> Correct" <|
+            \_ ->
+                let
+                    zipper : Zipper Province
+                    zipper =
+                        Zipper.from [ madrid ] alicante [ valencia ]
+                in
+                Expect.equal Correct (getProvinceStatus 1 zipper)
+        , test "province asked and answered incorrectly -> Failed" <|
+            \_ ->
+                let
+                    zipper : Zipper Province
+                    zipper =
+                        Zipper.from [ madrid, valencia ] alicante []
+                in
+                Expect.equal Failed (getProvinceStatus 3 zipper)
         ]
